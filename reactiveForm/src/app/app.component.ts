7fb15cb6e5d3324f37ece902b165c80237a83e5d
Nagadarshancnaik <nagdarshan.naik@robosoftin.com>
2022-10-13 18:11:49 +0530

    Your message

--- reactiveForm/src/app/app.component.ts
+++ reactiveForm/src/app/app.component.ts
@@ -15,10 +15,12 @@ export class AppComponent implements OnInit {
         biller:new FormControl(null,Validators.required),
         locationcode:new FormControl(null,Validators.required),
         rrno:new FormControl(null,Validators.required),
-        name:new FormControl(null,[Validators.required,Validators.pattern('^[a-zA-Z ]*$')]),
-        email:new FormControl(null,[Validators.required,Validators.email]),
-        mblno:new FormControl(null,[Validators.required,Validators.pattern("^[0-9]*$"),Validators.minLength(10),Validators.maxLength(10)]),
-        amount:new FormControl(null,Validators.required),
+        personalDetails: new FormGroup({
+          name:new FormControl(null,[Validators.required,Validators.pattern('^[a-zA-Z ]*$')]),
+          email:new FormControl(null,[Validators.required,Validators.email]),
+          mblno:new FormControl(null,[Validators.required,Validators.pattern("^[0-9]*$"),Validators.minLength(10),Validators.maxLength(10)]),
+        }),
+        amount:new FormControl(null,[Validators.required,Validators.pattern("^[0-9]*$")]),
         paymentMethod:new FormControl(null,Validators.required),
     });
   }
@@ -27,29 +29,29 @@ export class AppComponent implements OnInit {
     console.log(this.reactiveForm);
   }
 
-  get biller():FormControl{
-    return this.reactiveForm.get("biller") as FormControl;
-  }
+  // get biller():FormControl{
+  //   return this.reactiveForm.get("biller") as FormControl;
+  // }
 
-  get locationcode():FormControl{
-    return this.reactiveForm.get("locationcode") as FormControl;
-  }
-  get rrno():FormControl{
-    return this.reactiveForm.get("rrno") as FormControl;
-  }
-  get name():FormControl{
-    return this.reactiveForm.get("name") as FormControl;
-  }
-  get email():FormControl{
-    return this.reactiveForm.get("email") as FormControl;
-  }
-  get mblno():FormControl{
-    return this.reactiveForm.get("mblno") as FormControl;
-  }
-  get amount():FormControl{
-    return this.reactiveForm.get("amount") as FormControl;
-  }
-  get paymentMethod():FormControl{
-    return this.reactiveForm.get("paymentMethod") as FormControl;
-  }
+  // get locationcode():FormControl{
+  //   return this.reactiveForm.get("locationcode") as FormControl;
+  // }
+  // get rrno():FormControl{
+  //   return this.reactiveForm.get("rrno") as FormControl;
+  // }
+  // get name():FormControl{
+  //   return this.reactiveForm.get("name") as FormControl;
+  // }
+  // get email():FormControl{
+  //   return this.reactiveForm.get("email") as FormControl;
+  // }
+  // get mblno():FormControl{
+  //   return this.reactiveForm.get("mblno") as FormControl;
+  // }
+  // get amount():FormControl{
+  //   return this.reactiveForm.get("amount") as FormControl;
+  // }
+  // get paymentMethod():FormControl{
+  //   return this.reactiveForm.get("paymentMethod") as FormControl;
+  // }
 }
