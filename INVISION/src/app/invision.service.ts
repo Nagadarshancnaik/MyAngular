ed704e3780f3fc67f9435db3ffa307c6072cac20
Nagadarshancnaik <nagdarshan.naik@robosoftin.com>
2022-11-07 17:41:23 +0530

    .

--- INVISION/src/app/invision.service.ts
+++ INVISION/src/app/invision.service.ts
@@ -3,51 +3,76 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 
-interface login extends Array<login[]>{
- 
-  id:number,
+interface login extends Array<login[]> {
 
-  name:string,
+  id: number,
 
-  pass:string
+  name: string,
+
+  pass: string
+}
+
+interface addSuperAdmin extends Array<login[]> {
+
+  employeeName: string,
+  employeeCode: string,
+  employeeEmail: string,
+  id: number
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvisionService {
-  
-  constructor(private http:HttpClient) { }
-  
 
-  getpass():Observable<login[]>{
+  constructor(private http: HttpClient) { }
+
+
+  getpass(): Observable<login[]> {
     return (this.http.get<login[]>('http://localhost:3000/passwordManager'));
-   }
+  }
 
-  
 
-   loginCheck():Observable<login[]>{
+
+  loginCheck(): Observable<login[]> {
     return (this.http.get<login[]>('http://localhost:3000/passwordManager'));
-   }
+  }
 
-   updateEmployee(pass:any,name:any,id:any){
-    const body={
+  updateEmployee(pass: any, name: any, id: any) {
+    const body = {
       name: name,
       pass: pass,
-      id:id
+      id: id
     }
-    return this.http.put('http://localhost:3000/passwordManager/'+id,body);
-   }
+    return this.http.put('http://localhost:3000/passwordManager/' + id, body);
+  }
 
 
 
-   //super admin
-   postEmployeeDetail(data:any){
-    return this.http.post<any>('http://localhost:3000/EmployeeDetails/',data);
-   }
+  //super admin
+  postSuperAdminDetails(data: any) {
+    return this.http.post<any>('http://localhost:3000/SuperAdmin', data);
+  }
 
-   getEmployeeDetail(){
-    return this.http.get<any>('http://localhost:3000/EmployeeDetails/');
-   }
-   
+  getSuperAdminDetails() {
+    return this.http.get<any>('http://localhost:3000/SuperAdmin');
+  }
+
+
+  editSuperAdmin(data: any, id: any) {
+    const body = {
+      employeeName: data.employeeName,
+      employeeCode: data.employeeCode,
+      employeeEmail: data.employeeEmail,
+      id: id
+    }
+    return this.http.put('http://localhost:3000/SuperAdmin/' + id, body)
+  }
+
+
+  deleteSuperAdmin(id: any): Observable<login[]> {
+return this.http.delete<login[]>('http://localhost:3000/SuperAdmin/' + id);
+  }
+
+  
 }
--- INVISION/src/app/super-admin/super-admin.component.ts
+++ INVISION/src/app/super-admin/super-admin.component.ts
@@ -9,7 +9,10 @@ import { InvisionService } from '../invision.service';
 })
 export class SuperAdminComponent implements OnInit {
   superAdminForm!:FormGroup
-
+  superAdmin:any;
+  currentSuperAdmin:any;
+  editMode:boolean=false;
+  superAdminId:any;
   constructor(private fb:FormBuilder,private loginservice: InvisionService) { }
 
   ngOnInit(): void {
@@ -18,32 +21,68 @@ export class SuperAdminComponent implements OnInit {
       employeeCode:this.fb.control(null,Validators.required),
       employeeEmail:this.fb.control(null,Validators.required)
     });
-    this.getAllEmployeeDetails();
+    this.getAllSuperAdminDetails();
+    
+
   }
 
-  onSubmit(){
-    if(this.superAdminForm.valid){
-      this.loginservice.postEmployeeDetail(this.superAdminForm.value).subscribe({
+  addsuperAdmin(){
+    if(this.superAdminForm.valid&& !this.editMode){
+      this.loginservice.postSuperAdminDetails(this.superAdminForm.value).subscribe({
         next:(value)=> {
-          alert("details added");
           this.superAdminForm.reset();
+          alert("details added");
+          this. getAllSuperAdminDetails();
         },
         error:()=>{
           alert("error occured");
         }
       })
-    }
+        }
+        else{
+          this.loginservice.editSuperAdmin(this.superAdminForm.value,this.superAdminId).subscribe(data=>{
+            // this.loginservice.getSuperAdminDetails().subscribe(this.superAdmin=data);
+          this. getAllSuperAdminDetails();
+            this.editMode=false
+          })
+        }
+// this.editMode=false;
   }
 
-  getAllEmployeeDetails(){
-    this.loginservice.getEmployeeDetail().subscribe({
+  getAllSuperAdminDetails(){
+    this.loginservice.getSuperAdminDetails().subscribe({
       next:(value)=>{
         console.log(value);
+        this.superAdmin=value;
       },
       error:()=>{
         alert('error');
         
       }
     })
+  }
+
+
+  //edit super admin
+  editSuperAdmin(id:any){
+    this.superAdminId=id;
+    this.editMode=true;
+    this.currentSuperAdmin=this.superAdmin.find((d:any)=>{
+      return d.id==id;
+    });
+    console.log(this.currentSuperAdmin);
+    this.superAdminForm.setValue({
+      employeeName:this.currentSuperAdmin.employeeName,
+      employeeCode:this.currentSuperAdmin.employeeCode,
+      employeeEmail:this.currentSuperAdmin.employeeEmail,
+    })
+    // this. getAllSuperAdminDetails();
+
+  }
+
+  deleteSuperAdmin(id:any){
+this.loginservice.deleteSuperAdmin(id).subscribe(data=>{
+  this. getAllSuperAdminDetails();
+})
   }
 }
